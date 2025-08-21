3632cace63325b4d498b84b67d63009d52895434
Henry <henry.douzant@icetravelgroup.com>
2025-08-21 22:56:14 +0100

    Add Ai destination page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,6 +6,7 @@ import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
+import * as $travel_tips_destination_ from "./routes/travel-tips/[destination].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $Counter from "./islands/Counter.tsx";
 import type { Manifest } from "$fresh/server.ts";
@@ -16,6 +17,7 @@ const manifest = {
     "./routes/_app.tsx": $_app,
     "./routes/api/joke.ts": $api_joke,
     "./routes/greet/[name].tsx": $greet_name_,
+    "./routes/travel-tips/[destination].tsx": $travel_tips_destination_,
     "./routes/index.tsx": $index,
   },
   islands: {
